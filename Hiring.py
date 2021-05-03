4c83e24ddff084a540a81a90c8c36a6b9ade02b7
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-05-03 00:12:46 -0400

    Add files via upload

--- Hiring.py
+++ Hiring.py
@@ -4,29 +4,36 @@
 # Email Address: daij24@mcmaster.ca or david1147062956@163.co
 
 class Hiring:
-    def __init__(self, cost: float, maxCost=100, minCost=0):
+    def __init__(self, cost: int, cutoff: int, maxCost=100, minCost=0):
         if cost <= minCost:
             raise ValueError()
         elif cost >= maxCost:
             raise ValueError()
         elif minCost >= maxCost:
             raise ValueError()
-        self.cost = cost
-        self.applicants = []
+        self.__cost = cost
+        self.__applicants = []
+        self.__cutoff = cutoff
 
     def __str__(self):
         Part1 = " Hiring problem "
-        Part2 = " with cost " + str(self.cost)
-        if len(self.applicants) == 0:
+        Part2 = " with cost " + str(self.__cost)
+        if len(self.__applicants) == 0:
             Part3 = " without applicants "
         else:
             Part3 = " with applicants "
-            for i in self.applicants:
+            for i in self.__applicants:
                 Part3 += " " + str(i) + " "
         return Part1 + Part2 + Part3
 
     def getCost(self):
-        return self.cost
+        return self.__cost
 
     def getApplicants(self):
-        return self.applicants
+        return self.__applicants
+
+    def getCutoff(self):
+        return self.__cutoff
+
+    def addApplicants(self, app: int):
+        self.__applicants = self.__applicants + [app]
