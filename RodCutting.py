e2e0d8dba0d0ceb0c6ea925bf324956ecfe074cb
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-06-03 19:53:10 -0400

    Add files via upload

--- RodCutting.py
+++ RodCutting.py
@@ -1,3 +1,6 @@
+# Author: Jingze Dai
+# Date: 21/05/2021
+# Email Address: daij24@mcmaster.ca or david1147062956@163.com
 class RodCutting:
     def __init__(self, length: int, price: list):
         if length <= 0:
@@ -35,3 +38,50 @@ class RodCutting:
             self.__price = []
         except:
             raise Exception()
+
+    def Cut_Rod(self):
+        if self.__length == 0:
+            return 0
+        q = - 1000000
+        i = 0
+        while i < self.__length:
+            self.__length = self.__length - i
+            q = max(q, self.__price[i] + self.Cut_Rod())
+            i += 1
+        return q
+
+    def Mem_Cut_Rod(self):
+        i = 0
+        r = []
+        while i < self.__length:
+            r += [- 1000000]
+            i += 1
+        return self.Mem_Cut_Rod_Aux(r)
+
+    def Mem_Cut_Rod_Aux(self, r):
+        if r[self.__length] > 0:
+            return r[self.__length]
+        if self.__length == 0:
+            q = 0
+        else:
+            q = - 1000000
+            j = 0
+            while j < self.__length:
+                self.__length = self.__length - j
+                q = max(q, self.__price[j] + self.Mem_Cut_Rod_Aux(r))
+        r[self.__length] = q
+        return q
+
+    def Bottom_Up_Cut_Rod(self):
+        r = []
+        r += [0]
+        i = 0
+        while i < self.__length:
+            q = - 1000000
+            j = 0
+            while j < i:
+                q = max(q, self.__price[i] + r[j - i])
+                j += 1
+            r[i] = q
+            i += 1
+        return r[self.__length]
