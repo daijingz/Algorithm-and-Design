8239901c229f5c7f82eb646e802ccef32783615f
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-06-04 19:52:48 -0400

    Add files via upload

--- Approximation_Graph.py
+++ Approximation_Graph.py
@@ -5,25 +5,49 @@ from collections import defaultdict
 
 
 class Approximation_Graph:
-    def __init__(self): # Constructor Time Complexity: O(1) + O(1) = O(1)
+    def __init__(self):
+        """!
+        @return a graph object with bodies and nodes
+        """
         self.vertices = [] # Time Complexity: O(1)
         self.graph = defaultdict(list) # Time Complexity: O(1)
 
-    def get_vertices(self): # Method Time Complexity: O(1)
-        return self.vertices # Time Complexity: O(1)
+    # Constructor Total Time Complexity: O(1) + O(1) = O(1)
 
-    def get_graph(self): # Method Time Complexity: O(1)
-        return self.graph # Time Complexity: O(1)
+    def get_vertices(self):
+        """! Get vertices values of an object"""
+        try:
+            return self.vertices # Time Complexity: O(1)
+        except:
+            raise Exception()
 
-    def addEdge(self, vertex1, vertex2): # Method Time Complexity: O(1) + O(1) + O(1) + O(1) = O(1)
-        if self.vertices.count(vertex1) == 0: # Time Complexity: O(1)
-            self.vertices += [vertex1]
-        if self.vertices.count(vertex2) == 0: # Time Complexity: O(1)
-            self.vertices += [vertex2]
-        self.graph[vertex1].append(vertex2) # Time Complexity: O(1)
-        self.graph[vertex2].append(vertex1) # Time Complexity: O(1)
+    # Method Total Time Complexity: O(1)
 
-    def FindVertexCover(self): # Method Time Complexity: O(V + E)
+    def get_graph(self):
+        """! Get graph body values of an object"""
+        try:
+            return self.graph # Time Complexity: O(1)
+        except:
+            raise Exception()
+
+    # Method Total Time Complexity: O(1)
+
+    def addEdge(self, vertex1, vertex2):
+        """! Add a new edge to a new graph object"""
+        try:
+            if self.vertices.count(vertex1) == 0: # Time Complexity: O(1)
+                self.vertices += [vertex1]
+            if self.vertices.count(vertex2) == 0: # Time Complexity: O(1)
+                self.vertices += [vertex2]
+            self.graph[vertex1].append(vertex2) # Time Complexity: O(1)
+            self.graph[vertex2].append(vertex1) # Time Complexity: O(1)
+        except:
+            raise Exception()
+
+    # Method Total Time Complexity: O(1) + O(1) + O(1) + O(1) = O(1)
+
+    def FindVertexCover(self):
+        """! Find the smallest vertex cover of an undirected graph"""
         Visited = [False] * len(self.vertices) # Initially all nodes have not been visited
         Output = [] # Output shows the vertex set of vertex cover
         for vertex in range(len(self.vertices)):
@@ -37,17 +61,5 @@ class Approximation_Graph:
                 # Collect vertex to output list
                 Output += [vertex]
         return Output # Time Complexity: O(1)
-# Below are test cases: A graph object has a number of nodes and edges.
-# If you want to use other test cases, modify all lines of codes below
 
-
-Graph = Approximation_Graph()
-Graph.addEdge(0, 1)
-Graph.addEdge(2, 0)
-Graph.addEdge(2, 1)
-Graph.addEdge(1, 3)
-Graph.addEdge(1, 4)
-Graph.addEdge(4, 3)
-print(Graph.vertices)
-print(Graph.graph)
-print(Graph.FindVertexCover())
+    # Method Total Time Complexity: O(V + E)
