280b1af97cecfa223f0818fb218411e91e335695
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-06-12 16:47:05 -0400

    Add files via upload

---
+++ 2DArray3.py
@@ -0,0 +1,9 @@
+# Author: Jingze Dai
+# Date: 16/02/2021
+# Email Address: daij24@mcmaster.ca or david1147062956@163.com
+
+
+class Table:
+    def __init__(self, row: int, col: int):
+        self.row = row
+        self.col = col
