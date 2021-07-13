d64b0428efbaa8fc171ccc53931abd22cdef99de
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-07-13 12:43:54 -0400

    Add files via upload

--- RodCutting.py
+++ RodCutting.py
@@ -7,6 +7,11 @@
 
 class Rod:
     def __init__(self, length: int, price: list):
+        """!
+        Construct a Rod object for rod cutting algorithm
+        @:param length      length of rod
+        @:param price       price list of different length's rods
+        """
         if length <= 0:
             raise Exception()
 
@@ -25,18 +30,21 @@ class Rod:
         self.__price = price
 
     def get_length(self):
+        """! @return length of Rod object """
         try:
             return self.__length
         except:
             raise Exception()
 
     def get_price(self):
+        """! @return price list of Rod object """
         try:
             return self.__price
         except:
             raise Exception()
 
     def set_rod(self, length: int, price: list):
+        """! Reset rod values"""
         self.__length = length
 
         if len(price) > length:
@@ -52,6 +60,7 @@ class Rod:
         self.__price = price
 
     def empty_object(self):
+        """! Empty a rod object """
         try:
             self.__length = 0
             self.__price = []
@@ -59,53 +68,61 @@ class Rod:
             raise Exception()
 
     def __eq__(self, other):
+        """! Compare 2 Rod objects to check whether they are equal """
         if isinstance(other, Rod):
             return self.__length == other.__length and self.__price == other.__price
         return False
 
     def Cut_Rod(self):
-        if self.__length == 0:
+        """! Cut rod algorithm implementations """
+        if self.__length <= 0:
             return 0
-        q = - 1000000
-        i = 0
+        q, i = 0, 0
+        length = self.__length
+        price = self.__price
         while i < self.__length:
-            self.__length = self.__length - i
+            self.__length = self.__length - i - 1
             q = max(q, self.__price[i] + self.Cut_Rod())
             i += 1
+            self.__length = length
+            self.__price = price
         return q
 
     def Mem_Cut_Rod(self):
+        """! Memorized cut rod algorithm implementations """
         i = 0
         r = []
         while i < self.__length:
-            r += [- 1000000]
+            r += [0]
             i += 1
         return self.Mem_Cut_Rod_Aux(r)
 
     def Mem_Cut_Rod_Aux(self, r):
-        if r[self.__length] > 0:
+        """! Helper function of memorized cut rod algorithm implementations """
+        if self.__length < len(r) and r[self.__length] > 0:
             return r[self.__length]
-        if self.__length == 0:
+        if self.__length <= 0:
             q = 0
         else:
             q = - 1000000
             j = 0
+            length = self.__length
+            price = self.__price
             while j < self.__length:
-                self.__length = self.__length - j
+                self.__length = self.__length - j - 1
                 q = max(q, self.__price[j] + self.Mem_Cut_Rod_Aux(r))
-        r[self.__length] = q
+                j += 1
+                self.__length = length
+                self.__price = price
+        r += [q]
         return q
 
-    def Bottom_Up_Cut_Rod(self):
-        r = []
-        r += [0]
+    def analysis(self):
+        """! Rod cutting problem analyzing """
         i = 0
-        while i < self.__length:
-            q = - 1000000
-            j = 0
-            while j < i:
-                q = max(q, self.__price[i] + r[j - i])
-                j += 1
-            r[i] = q
+        output = []
+        while i < len(self.__price):
+            output += [self.__price[i] / (i + 1)]
             i += 1
-        return r[self.__length]
+        output.sort()
+        return output
