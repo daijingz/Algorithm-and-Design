8bfebfd1d555c728023d2ecb000a021925ef2230
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-07-26 10:52:37 -0400

    Add files via upload

---
+++ QuickFind.py
@@ -0,0 +1,5 @@
+# Author: Jingze Dai
+# Email Address: daij24@mcmaster.ca or david1147062956@163.com
+# Github: https://github.com/daijingz
+# Linkedin: https://www.linkedin.com/in/jingze-dai/
+# Description: Quick Find
