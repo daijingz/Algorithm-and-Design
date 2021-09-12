45a5df20e2d29408e101d3ee8f1c5510a55b315e
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2021-09-12 17:48:45 -0400

    Add files via upload

--- Bag.py
+++ Bag.py
@@ -3,3 +3,18 @@
 # Github: https://github.com/daijingz
 # Linkedin: https://www.linkedin.com/in/jingze-dai/
 # Description: Bag basic Implementation
+
+
+class Bag:
+    def __init__(self):
+        self.body = ()
+
+    def add(self, item):
+        new_body = self.body + (item, )
+        self.body = new_body
+
+    def isEmpty(self):
+        return len(self.body) == 0
+
+    def size(self):
+        return len(self.body)
