e357b70849acb39c1365c91e2a001791d883961b
Jingze Dai <61481741+daijingz@users.noreply.github.com>
2022-03-13 19:22:09 -0400

    Delete Module 1 Self-Introduction directory

--- Module 1 Self-Introduction/Official_JSON_File.py
+++
@@ -1,5 +0,0 @@
-# Author: Jingze Dai
-# Email Address: daij24@mcmaster.ca or david1147062956@163.com
-# Github: https://github.com/daijingz
-# Linkedin: https://www.linkedin.com/in/jingze-dai/
-# Description: JSON Introduction Page
